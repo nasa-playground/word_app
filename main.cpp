d51091a9fd3a00c59e574578f4c970adccfceb75
nasa <htilcs1115@gmail.com>
2019-01-28 16:51:35 +0900

    add: 大枠を追加

--- main.cpp
+++ main.cpp
@@ -4,6 +4,23 @@
 #include <string>
 using namespace std;
 
-int main(void) {
+int main() {
+  // input file
 
+
+  // Serialize data
+  // List<Map<String, String>>
+
+  // イメージこんな感じ
+  // for() {
+  //   if(correct)
+  //     cout << "正解\n";
+  //   else
+  //     cout << "不正解\n";
+  // }
+
+  cout << "終了!\n";
+
+  // 正解率の表示
+  // cout << "正答数:" << hoge << "/" << hoge << '\n';
 }
