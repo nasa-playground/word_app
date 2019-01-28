56dadf52242f8e1227e74e5573165379e3d37b0c
nasa <htilcs1115@gmail.com>
2019-01-28 17:27:23 +0900

    終わり

--- main.cpp
+++ main.cpp
@@ -1,26 +1,79 @@
 #include <iostream>
+#include <fstream>
 #include <map>
-#include <list>
 #include <string>
+#include <vector>
 using namespace std;
 
-int main() {
+typedef string Ja;
+typedef string En;
+typedef map<Ja,En> Dict;
+typedef vector<Dict> Dicts;
+
+vector<std::string> split(const std::string &str, char sep);
+map<string, string> analyze_file(string str);
+
+int main(int argc,char *argv[]) {
+  string filename = argv[1];
+  cout << filename;
+
   // input file
+  std::ifstream ifs(filename);
+  std::string str;
+  if (ifs.fail()) {
+    std::cerr << "Failed: load file" << std::endl;
+    return -1;
+  }
 
+  cout << str << '\n';
 
-  // Serialize data
-  // List<Map<String, String>>
+  Dicts dicts;
+  while (getline(ifs, str)) {
+    dicts.push_back(analyze_file(str));
+  }
 
-  // イメージこんな感じ
-  // for() {
-  //   if(correct)
-  //     cout << "正解\n";
-  //   else
-  //     cout << "不正解\n";
-  // }
+  for(auto dict = dicts.rbegin(); dict != dicts.rend(); ++dict) {
+    for(auto itr = dict->begin(); itr != dict->end(); ++itr) {
+      cout << itr->second<< "の英語は？";
 
-  cout << "終了!\n";
+      string answer;
+      cin >>  answer;
 
-  // 正解率の表示
-  // cout << "正答数:" << hoge << "/" << hoge << '\n';
+      if(answer == itr->first)
+        cout << "正解\n";
+      else
+        cout << "不正解\n";
+    }
+  }
+
+  cout << "おつおつ!\n";
+}
+
+// ここは適当にコピペしました
+vector<std::string> split(const std::string &str, char sep) {
+    std::vector<std::string> v;
+    auto first = str.begin();
+    while( first != str.end() ) {
+        auto last = first;
+        while( last != str.end() && *last != sep )
+            ++last;
+        v.push_back(std::string(first, last));
+        if( last != str.end() )
+            ++last;
+        first = last;
+    }
+    return v;
+}
+
+map<string, string> analyze_file(string str) {
+  vector<string> v = split(str, ':');
+
+  En en = v[0];
+  Ja ja = v[1];
+
+  Dict dict;
+
+  dict[en] = ja;
+
+  return dict;
 }
